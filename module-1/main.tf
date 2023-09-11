2dbd83399ec78e871b75f436388c1df64f3c0440
Aravindh Subramanian <j.aravindhrajkumar@gmail.com>
2023-09-10 21:10:05 -0400

    modified to remove the duplocate values

--- module-1/main.tf
+++ module-1/main.tf
@@ -2,7 +2,7 @@
 # You can use it to encapsulate logic or calculations.
 
 # Example: Concatenate two strings and store the result in an output variable.
-output "concatenated_strings" {
-  description = "The result of concatenating two strings."
-  value       = var.string1_value != "" ? "${var.string1_value} - ${var.string2_value}" : "No strings provided"
-}
+# output "concatenated_strings" {
+#   description = "The result of concatenating two strings."
+#   value       = var.string1_value != "" ? "${var.string1_value} - ${var.string2_value}" : "No strings provided"
+# }
